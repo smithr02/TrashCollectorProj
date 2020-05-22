332d6e42daa157832f359615d5d24156d9cb5e60
Ryan Smith <ryan_smith2@hotmail.com>
2020-05-22 13:40:15 -0500

    added action filters folder

--- TrashCollector/Startup.cs
+++ TrashCollector/Startup.cs
@@ -12,6 +12,8 @@ using TrashCollector.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace TrashCollector
 {
@@ -34,6 +36,12 @@ namespace TrashCollector
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
+            services.AddScoped<ClaimsPrincipal>(s =>
+            s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
