e610a7be742e2de5daf2f2984f4e74637bbeb238
Ryan Smith <ryan_smith2@hotmail.com>
2020-05-24 12:40:01 -0500

    added logic to the employees controller for add, edit and delete.

--- TrashCollector/Controllers/EmployeesController.cs
+++ TrashCollector/Controllers/EmployeesController.cs
@@ -4,21 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TrashCollector.Data;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class EmployeesController : Controller
+        
     {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: EmployeesController
         public ActionResult Index()
         {
-            return View();
+            var employees = _context.Employees.ToList();
+            return View(employees);
         }
 
         // GET: EmployeesController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var employees = _context.Employees.Where(e => e.Id == id).SingleOrDefault();
+            return View(employees);
         }
 
         // GET: EmployeesController/Create
@@ -30,10 +41,12 @@ namespace TrashCollector.Controllers
         // POST: EmployeesController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Employee employee)
         {
             try
             {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -45,16 +58,23 @@ namespace TrashCollector.Controllers
         // GET: EmployeesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var employees = _context.Employees.Where(e => e.Id == id).SingleOrDefault();
+            return View(employees);
         }
 
         // POST: EmployeesController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Employee employee)
         {
             try
             {
+                var employeeFromDb = _context.Employees.Where(e => e.Id == id).FirstOrDefault();
+                employeeFromDb.FirstName = employee.FirstName;
+                employeeFromDb.LastName = employee.LastName;
+                employeeFromDb.ZipCode = employee.ZipCode;
+                _context.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -66,16 +86,19 @@ namespace TrashCollector.Controllers
         // GET: EmployeesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var employees = _context.Employees.Where(e => e.Id == id).SingleOrDefault();
+            return View(employees);
         }
 
         // POST: EmployeesController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Employee employee)
         {
             try
             {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
