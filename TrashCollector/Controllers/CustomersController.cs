1d154b80299db4c476bfe550263de2da8e2fa3ac
Ryan Smith <ryan_smith2@hotmail.com>
2020-05-24 18:29:29 -0500

    Added logic to customercontroller, add, edit and delete

--- TrashCollector/Controllers/CustomersController.cs
+++ TrashCollector/Controllers/CustomersController.cs
@@ -4,21 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TrashCollector.Data;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: CustomersController
         public ActionResult Index()
         {
-            return View();
+            var customers = _context.Customers.ToList();
+            return View(customers);
         }
 
         // GET: CustomersController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var customers = _context.Employees.Where(c => c.Id == id).SingleOrDefault();
+            return View(customers);
         }
 
         // GET: CustomersController/Create
@@ -30,10 +39,12 @@ namespace TrashCollector.Controllers
         // POST: CustomersController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Customer customer)
         {
             try
             {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -45,16 +56,26 @@ namespace TrashCollector.Controllers
         // GET: CustomersController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var customers = _context.Customers.Where(c => c.Id == id).SingleOrDefault();
+            return View(customers);
         }
 
         // POST: CustomersController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Customer customer)
         {
             try
             {
+                var customerFromDb = _context.Customers.Where(c => c.Id == id).FirstOrDefault();
+                customerFromDb.FirstName = customer.FirstName;
+                customerFromDb.LastName = customer.LastName;
+                customerFromDb.StreetNumber = customer.StreetNumber;
+                customerFromDb.StreetName = customer.StreetName;
+                customerFromDb.State = customer.State;
+                customerFromDb.ZipCode = customer.ZipCode;
+                _context.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -66,16 +87,19 @@ namespace TrashCollector.Controllers
         // GET: CustomersController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var customers = _context.Customers.Where(c => c.Id == id).SingleOrDefault();
+            return View(customers);
         }
 
         // POST: CustomersController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Customer customer)
         {
             try
             {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
